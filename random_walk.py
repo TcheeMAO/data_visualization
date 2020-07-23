15008fa27b93782f838e66e0bfc7aab9e4173080
Timothy O <timothy.oei@gmail.com>
2020-07-23 19:06:54 -0400

    Forgot to add random_walk.py to staging area for the last commit

--- random_walk.py
+++ random_walk.py
@@ -14,7 +14,6 @@ class RandomWalk():
 
     def fill_walk(self):
         """Calculate all the points in the walk."""
-
         # Keep taking steps until the walk reaches the desired length.
         while len(self.x_values) < self.num_points:
             # Decide which direction to go and how far to go in that direction.
